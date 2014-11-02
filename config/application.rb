ba1a7f4ad497001e3c6031a8a8bf13e471379443
Nishad Singh <nishadsingh@berkeley.edu>
2014-11-01 21:31:46 -0700

    description length now a global variable

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module DareKickstarter
   class Application < Rails::Application
+    # Site-wide global variables related to Dares.
+    config.min_description_length = 10
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
