0204c4b2c6acfef0856196855183d4f4f99d51e7
Nishad Singh <nishadsingh@berkeley.edu>
2014-11-01 21:35:45 -0700

    Fund and Wallet validation updated

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -2,4 +2,7 @@ class Fund < ActiveRecord::Base
   belongs_to :dare
   belongs_to :user
   validates :points, presence: true
+  validates :user, presence: true
+  validates :dare, presence: true
+
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,4 +2,6 @@ class Wallet < ActiveRecord::Base
   belongs_to :user
   has_many :funds, :through => :user
   validates :points, presence: true
+  validates :user, presence: true
+  
 end
