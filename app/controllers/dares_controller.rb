4a9011d24720de551025b779b2397e928c4eb239
DarryQueen <slimfrog@gmail.com>
2014-11-01 22:15:28 -0700

    Added Dare creation form.

--- app/controllers/dares_controller.rb
+++ app/controllers/dares_controller.rb
@@ -1,4 +1,25 @@
 class DaresController < ApplicationController
-    def new
+  def new
+    @dare = Dare.new
+  end
+
+  def create
+    @dare = Dare.new(dare_params)
+    @dare.creator = current_user
+
+    @dare.save
+    if @dare.save
+      redirect_to @dare
+    else
+      render 'new'
     end
+  end
+
+  def index
+  end
+
+  private
+  def dare_params
+    params.require(:dare).permit(:title, :description)
+  end
 end
