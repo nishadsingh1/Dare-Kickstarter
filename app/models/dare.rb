565d81c633d9eb6900466cc4a34a6a58c1bf8e40
DarryQueen <slimfrog@gmail.com>
2014-11-01 19:49:34 -0700

    Added Fund model.

--- app/models/dare.rb
+++ app/models/dare.rb
@@ -1,2 +1,3 @@
 class Dare < ActiveRecord::Base
+  has_many :funds, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102011451) do
+ActiveRecord::Schema.define(version: 20141102024016) do
 
   create_table "dares", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "goal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "funds", force: true do |t|
+    t.integer  "points"
+    t.integer  "dare_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
