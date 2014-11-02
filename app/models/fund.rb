ad359c9b40aae56ce2feabbd9f26e8586036d066
Nishad Singh <nishadsingh@berkeley.edu>
2014-11-01 21:19:08 -0700

    added more model validation

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,4 +1,5 @@
 class Fund < ActiveRecord::Base
   belongs_to :dare
   belongs_to :user
+  validates :points, presence: true
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,5 @@
 class Wallet < ActiveRecord::Base
   belongs_to :user
   has_many :funds, :through => :user
+  validates :points, presence: true
 end
