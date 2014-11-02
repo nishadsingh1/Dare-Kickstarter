bd201643f994b2fe535ca0daa40cad74d2e3e5e6
Nishad Singh <nishadsingh@berkeley.edu>
2014-11-01 21:33:24 -0700

    validation for Dares changed

--- app/models/dare.rb
+++ app/models/dare.rb
@@ -2,8 +2,8 @@ class Dare < ActiveRecord::Base
   has_many :funds, dependent: :destroy
   belongs_to :creator, :foreign_key => "creator_id", :class_name => "User"
   belongs_to :subject, :foreign_key => "subject_id", :class_name => "User"
-  validates :description, length: { minimum: 10 }
+  validates :description, length: { minimum: DareKickstarter::Application.config.min_description_length }
   validates :title, presence: true
-  validates :creator_id, presence: true
+  validates :creator, presence: true
 
 end
